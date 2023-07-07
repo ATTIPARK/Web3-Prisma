cddd24e8de48e1243a333ec47d5825840d310b43
ATTIPARK <qkrqjawls0922@gmail.com>
2023-07-07 13:13:26 +0900

    first commit

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,38 @@
+import { prisma } from "@/app/lib/server";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  try {
+    const { account, email, signedToken } = await req.json();
+
+    const user = await prisma.user.upsert({
+      where: { account },
+      update: { signedToken },
+      create: {
+        account,
+        email,
+        signedToken,
+      },
+      select: {
+        account: true,
+        email: true,
+        nickname: true,
+        signedToken: true,
+      },
+    });
+
+    return NextResponse.json({
+      ok: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const GET = async () => {
+  try {
+  } catch (error) {
+    console.error(error);
+  }
+};
