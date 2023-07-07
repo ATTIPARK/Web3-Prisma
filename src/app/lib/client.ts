4dd673101539176509523d01293d796fef2877d5
ATTIPARK <qkrqjawls0922@gmail.com>
2023-07-07 14:15:21 +0900

    modify6

--- src/app/lib/client.ts
+++ src/app/lib/client.ts
@@ -1,6 +1,14 @@
+import { MetaMaskSDK } from "@metamask/sdk";
 import Web3, { Personal } from "web3";
 
-export const web3 = new Web3(typeof window !== "undefined" && window.ethereum);
-export const personal = new Personal(
-  typeof window !== "undefined" && window.ethereum
-);
+const MMSDK = new MetaMaskSDK({
+  dappMetadata: {
+    name: "Project",
+    url: "https://project.project",
+  },
+});
+
+const ethereum = MMSDK.getProvider();
+
+export const web3 = new Web3(ethereum);
+export const personal = new Personal(ethereum);
