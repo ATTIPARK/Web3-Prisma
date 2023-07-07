fd32a06345b162777e1c7b1cbf6ed67ecc10324c
ATTIPARK <qkrqjawls0922@gmail.com>
2023-07-07 13:56:47 +0900

    modify3

--- src/app/lib/client.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import Web3, { Personal } from "web3";
-
-export const web3 = new Web3(typeof window !== "undefined" && window.ethereum);
-export const personal = new Personal(
-  typeof window !== "undefined" && window.ethereum
-);
